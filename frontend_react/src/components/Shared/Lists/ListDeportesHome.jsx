590124060010e0f62d8c2873e13503d37041e6a2
GuiVelMat <gvm0898@gmail.com>
2025-01-04 19:47:01 +0100

    Frontend_React. Cargando info desde el context en ListDeportesHome

--- frontend_react/src/components/Shared/Lists/ListDeportesHome.jsx
+++ frontend_react/src/components/Shared/Lists/ListDeportesHome.jsx
@@ -1,23 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import CardDeportes from '../Cards/CardDeportes';
-import deporteService from '../../../services/Client/deportes.service';
+import Context from '../../../context/DeportesContext';
 import './ListDeportesHome.css';
 
 const ListDeportesHome = () => {
-    const [deportes, setDeportes] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const { deportes } = useContext(Context);
 
-    useEffect(() => {
-        const fetchDeportes = async () => {
-            const { data, loading } = await deporteService.GetDeportes();
-            console.log(data.deportes);
-            setDeportes(data.deportes);
-            setLoading(loading);
-        };
-
-        fetchDeportes();
-    }, []);
-    if (loading) return <p>Loading...</p>;
+    if (!deportes || deportes.length === 0) return <p>Loading...</p>;
 
     return (
         <div className="body-deportes-home">
@@ -29,7 +18,7 @@ const ListDeportesHome = () => {
                 </div>
             </div>
             <div className="row justify-content-center">
-                {deportes.slice(0, 8).map((deporte) => (
+                {deportes.deportes.slice(0, 8).map((deporte) => (
                     <CardDeportes key={deporte.id} deporte={deporte} />
                 ))}
             </div>
